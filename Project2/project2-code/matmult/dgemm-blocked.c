90e31aff96f8b1917d3e93d7dd76f125d3664e63
Pratyai Mazumder <pratyai.mazumder@gmail.com>
2021-09-25 23:02:40 +0200

    why???

--- Project2/project2-code/matmult/dgemm-blocked.c
+++ Project2/project2-code/matmult/dgemm-blocked.c
@@ -19,12 +19,13 @@ $(MKLROOT)/lib/intel64/libmkl_sequential.a $(MKLROOT)/lib/intel64/libmkl_core.a
 
 #include <stdio.h>
 
-static inline int min(int u, int v) { return u < v ? u : v; }
-static inline void swap(double *u, double *v) {
-  register const double tmp = *u;
-  *u = *v;
-  *v = tmp;
-}
+#define min(x, y) ((x) < (y) ? (x) : (y))
+#define swap(x, y)                                                             \
+  {                                                                            \
+    double tmp = (x);                                                          \
+    (x) = (y);                                                                 \
+    (y) = tmp;                                                                 \
+  }
 static inline int colmajor(int n, int i, int j) { return i + j * n; }
 static inline int rowmajor(int n, int i, int j) { return i * n + j; }
 static inline void print(int n, double *X, short colmaj) {
@@ -36,31 +37,10 @@ static inline void print(int n, double *X, short colmaj) {
   }
   printf("\n");
 }
-
 static inline void transpose_square(int n, double *X) {
   for (int i = 0; i < n; ++i) {
     for (int j = 0; j < i; ++j) {
-      swap(&X[colmajor(n, i, j)], &X[rowmajor(n, i, j)]);
-    }
-  }
-}
-
-// C[uw] += A[uv] * B[vw]
-// A in row major, B, C in column major.
-static inline void compute_for_block_combination(int n, double *A, double *B,
-                                                 double *C, int rA, int cA,
-                                                 int rB, int cB, int rC, int cC,
-                                                 int u, int v, int w) {
-  for (int i = 0; i < u; ++i) {
-    const int a_start = rowmajor(n, rA + i, cA);
-    for (int j = 0; j < w; ++j) {
-      const int b_start = colmajor(n, rB, cB + j);
-      const int c_at = colmajor(n, rC + i, cC + j);
-      register double c_ij = C[c_at];
-      for (int k = 0; k < v; ++k) {
-        c_ij += A[a_start + k] * B[b_start + k];
-      }
-      C[c_at] = c_ij;
+      swap(X[colmajor(n, i, j)], X[rowmajor(n, i, j)]);
     }
   }
 }
@@ -75,25 +55,26 @@ static void blocked_dgemm(int n, double *A, double *B, double *C,
   // to `X[i][j]` in memory, but `X[i][j+1]` is far away). So, we want to
   // iterate over `i` in the innermost loop to keep temporaly close accesses
   // also spacially close.
-  for (register int k = 0; k < n; k += block_size) {
-    const register int kl = min(k + block_size, n);
-    for (register int j = 0; j < n; j += block_size) {
-      const register int jl = min(j + block_size, n);
-      for (register int i = 0; i < n; i += block_size) {
-        const register int il = min(i + block_size, n);
-        for (register int kk = k; kk < kl; ++kk) {
-          register double *const a_start = A + kk * n;
-          for (register int jj = j; jj < jl; ++jj) {
-            register double *const c_start = C + jj * n;
-            const register double b_kj = B[kk + jj * n];
-            for (register int ii = i; ii < il; ++ii) {
-              c_start[ii] += a_start[ii] * b_kj;
+  transpose_square(n, A);
+  for (int j = 0; j < n; j += block_size) {
+    const int jl = min(j + block_size, n);
+    for (int i = 0; i < n; i += block_size) {
+      const int il = min(i + block_size, n);
+      for (int k = 0; k < n; k += block_size) {
+        const int kl = min(k + block_size, n);
+        for (int ii = i; ii < il; ++ii) {
+          for (int jj = j; jj < jl; ++jj) {
+            double c_ij = C[ii + jj * n];
+            for (int kk = k; kk < kl; ++kk) {
+              c_ij += A[ii*n + kk] * B[kk + jj * n];
             }
+            C[ii + jj * n] = c_ij;
           }
         }
       }
     }
   }
+  transpose_square(n, A);
 }
 
 static void naive_dgemm(int n, double *A, double *B, double *C) {
@@ -117,6 +98,5 @@ const char *dgemm_desc = "Blocked dgemm [pratyai].";
  * On exit, A and B maintain their input values. */
 void square_dgemm(int n, double *A, double *B, double *C) {
   // naive_dgemm(n, A, B, C);
-  // single_file_dgemm(n, A, B, C);
-  blocked_dgemm(n, A, B, C, 10);
+  blocked_dgemm(n, A, B, C, 128);
 }
